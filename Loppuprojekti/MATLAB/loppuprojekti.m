d89bb89e383d369dd7f99f7d393057540d352e4e
elli kiiski <kiiski.elli@gmail.com>
2023-09-20 11:00:06 +0300

    loppuprokkikselle tiedosto

---
+++ Loppuprojekti/MATLAB/loppuprojekti.m
@@ -0,0 +1,8 @@
+%% PROJEKTITYÖ - STEGANOGRAFIA
+% Valokuvan matematiikka 2023
+% Elli Kiiski
+%
+% Tämä skripti löytyy myös osoitteesta
+% https://github.com/ellikiiski/MATLAB-dump/tree/main/2023%20Valokuvan%20matematiikka/Loppuprojekti/MATLAB
+% suosittelen kloonaamaan sieltä, jotta käytettävät kuvat on
+% automaattisesti oikeassa kansiossa
